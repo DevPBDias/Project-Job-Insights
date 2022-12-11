437a27aad96bad23b6f90c780b2de81570ebc1c1
DevPBDias <devpbdias@gmail.com>
2022-12-11 18:34:12 -0300

    feat: get_min_salary function created

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -14,21 +14,14 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data = read(path)
+    return min(
+        [
+            int(salary["min_salary"])
+            for salary in data
+            if (salary["min_salary"]).isdigit()
+        ]
+    )
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
