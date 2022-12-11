59e65ab6fd5a5c03ea6710e49bd2d306781d2916
DevPBDias <devpbdias@gmail.com>
2022-12-11 18:32:48 -0300

    feat: get_max_salary function created

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,16 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data = read(path)
+    return max(
+        [
+            int(salary["max_salary"])
+            for salary in data
+            if (salary["max_salary"]).isdigit()
+        ]
+    )
 
 
 def get_min_salary(path: str) -> int:
@@ -64,8 +58,7 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
 
